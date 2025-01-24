f3eda17c0ea707ced0c559b2064a77938239c103
SanjayS05 <sanjaysankar2nd@gmail.com>
2025-01-25 00:01:21 +0530

    Working on creating post Refreshing works!

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,22 +1,21 @@
 import { Link } from 'react-router-dom';
-import { useEffect, useState, useContext } from 'react';
+import { useEffect, useContext } from 'react';
 import axios from 'axios';
 import { UserContext } from './userContext';
 
-export default function header()
-{
+export default function Header() {
     const { setUserInfo, userInfo } = useContext(UserContext);    
     useEffect(() => {
         axios.get('http://localhost:3000/profile', { withCredentials: true })
           .then(res => {
-            setUserInfo(res)
+            setUserInfo(res.data); 
           })
           .catch(error => {
             console.error('Error fetching profile:', error);
           });
-      }, [setUserInfo]);
+    }, [setUserInfo]);
 
-      function logout() {
+    function logout() {
         axios.post('http://localhost:3000/logout', {}, { withCredentials: true })
           .then(() => {
             setUserInfo(null);
@@ -24,28 +23,25 @@ export default function header()
           .catch(error => {
             console.error('Error logging out:', error);
           });
-      }
+    }
 
-    const username = userInfo?.username
+    const username = userInfo?.username;
 
     return (
         <header>
             <Link to="/" className='logo'>MyBlog</Link>
             <nav>
-                {username && (
+                {username ? (
                     <>
                         <Link to='/create'>Create new post</Link>
                         <a onClick={logout}>Logout</a>
                     </>
-                )}
-                {!username && (
+                ) : (
                     <>
                         <Link to="/login">Login</Link>
                         <Link to="/register">Register</Link>
                     </>
                 )}
             </nav>
-        </header>
-    );
-}
-
+        </header>)
+};
