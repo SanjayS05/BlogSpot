0f90e9b11ebb39e3bb8a0b53b3254f470c69f736
SanjayS05 <sanjaysankar2nd@gmail.com>
2025-01-24 15:45:35 +0530

    Main page started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,8 +6,6 @@ import Layout from './components/layout';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Routes>
       <Route path="/" element={ <Layout /> }>
