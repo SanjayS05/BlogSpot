0eac857f6574fe3e99e144432b186e2cf07becfd
SanjayS05 <sanjaysankar2nd@gmail.com>
2025-01-25 05:48:04 +0530

    Reapply "css"
    This reverts commit 594bf48955e2667bb8d9ff4f696666551ccaa706.

--- client/src/pages/postPage.jsx
+++ client/src/pages/postPage.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { format } from "date-fns";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { UserContext } from "../components/userContext";
 
 export default function PostPage() {
   const [postInfo, setPostInfo] = useState(null);
+  const {userInfo} = useContext(UserContext);
   const { id } = useParams();
 
   useEffect(() => {
@@ -24,6 +26,13 @@ export default function PostPage() {
         <h1>{postInfo.title}</h1> 
         <time>{format(new Date(postInfo.createdAt), 'MMM d, yyyy HH:mm')}</time>
         <div className="author">by @{postInfo.author.username}</div>
+        {userInfo.id === postInfo.author._id && (
+            <div className="edit-row">
+                <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
+                    <a href="" className="edit-btn">Edit this Post</a>
+                </Link>
+            </div>
+        )}
         <div className="image">
             <img src={`http://localhost:3000/${postInfo.cover}`} alt="" />
         </div>
